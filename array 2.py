066b990647d32dd3a042dded2a9cba86fb1006e4
carlos16153 <uribeblancocarlosadrian@gmail.com>
2024-07-27 09:26:07 -0400

    guarda

---
+++ array 2.py
@@ -0,0 +1,8 @@
+resp = ""
+while (resp != "0"):
+    print("1. Agregar elemento al inicio")
+    print("2. Agregar elemento al final")
+    print("3. Quitar último elemento")
+    print("4. Quitar primer elemento")
+    print("5. Ordenar Array")
+    resp = input("Seleccione una opción")
---
+++ my_var=[1,2,3,93,65,25,67,254,76,345].py
@@ -0,0 +1,13 @@
+resp = ""
+while (resp != "0"):
+    print("1. Agregar elemento al inicio")
+    print("2. Agregar elemento al final")
+    print("3. Quitar último elemento")
+    print("4. Quitar primer elemento")
+    print("5. Ordenar Array")
+    resp = input("Seleccione una opción")
+my_var=[1,2,3,93,55,25,67,254,76,101]
+nom=[["adrian","melani","pablo","jose"],
+      ["jorge","wilkern","david"]]
+print(f"hola {nom [1][2]}")
+print(f"numeros {my_var [4]}")
